844d9a64a191c1ae17e6d7a9c60a6636a0d33c5d
Peixoto07 <lucaspeixotodacruz@gmail.com>
2023-04-04 20:27:29 -0300

    lista ranking adcionada

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 
 const divCadastro = document.getElementsByClassName("modal_cadastro")
 const divPontuação = document.getElementsByClassName("modal_pontuação")
-const corpoTabela = document.getElementById('corpoTabela')
+const corpoTabelaCadastro = document.getElementById('corpoTabela')
 const nomeCadastro = document.getElementById("nome_usuario")
 const nomePontuacao = document.getElementById("nome_pontuacao")
 const btnPontuacao = document.getElementsByClassName("btnPontuacao")
@@ -15,6 +15,7 @@ const defesa = document.getElementById('defesa')
 const btn = document.getElementsByClassName("btAtualizar")
 const pagCadastro = document.getElementById("conteudo_principal")
 const pagRanking = document.getElementById("conteudo_ranking")
+const corpoTabelaRanking = document.getElementById('corpoTabelaRanking')
 
 const nomePrimeiroColocado = document.getElementById("nome_primeiro_colocado")
 const pontosPrimeiroColocado = document.getElementById("pontos_primeiro_colocado")
@@ -25,35 +26,37 @@ const pontosTerceiroColocado = document.getElementById("pontos_terceiro_colocado
 const alerta = document.getElementById("alerta")
 
 
-const ranking = ()=>{
+const ranking = () => {
     const jogadoresOrdenados = getLocalStorage().sort((a, b) => b.total - a.total)
-    const totalMaiorQueZero = jogadoresOrdenados.slice(0,3).every(jogador =>jogador.total>0)
-   if (jogadoresOrdenados.length>= 3 && totalMaiorQueZero) {
-       nomePrimeiroColocado.innerText=jogadoresOrdenados[0].nome
-       pontosPrimeiroColocado.innerText = jogadoresOrdenados[0].total
-       
-       nomeSegundoColocado.innerText=jogadoresOrdenados[1].nome
-       pontosSegundoColocado.innerText = jogadoresOrdenados[1].total
-       
-       nomeTerceiroColocado.innerText=jogadoresOrdenados[2].nome
-       pontosTerceiroColocado.innerText = jogadoresOrdenados[2].total
+    const totalMaiorQueZero = jogadoresOrdenados.filter(jogador => jogador.total > 0)
+    if (jogadoresOrdenados.length >= 3 && totalMaiorQueZero) {
+        nomePrimeiroColocado.innerText = jogadoresOrdenados[0].nome
+        pontosPrimeiroColocado.innerText = jogadoresOrdenados[0].total
 
-       trocaPagina(pagRanking,pagCadastro)
-   }else{
-    abreAlerta(`JOGOU AONDE?
-    Pontue com no mínimo 3 jogadores`,"#e65555")
-   }
+        nomeSegundoColocado.innerText = jogadoresOrdenados[1].nome
+        pontosSegundoColocado.innerText = jogadoresOrdenados[1].total
+
+        nomeTerceiroColocado.innerText = jogadoresOrdenados[2].nome
+        pontosTerceiroColocado.innerText = jogadoresOrdenados[2].total
+
+        limpaTabela(corpoTabelaRanking)
+        totalMaiorQueZero.forEach(criaLinhaRanking)
+        trocaPagina(pagRanking, pagCadastro)
+    } else {
+        abreAlerta(`JOGOU AONDE?
+    Pontue com no mínimo 3 jogadores`, "#e65555")
+    }
 }
 
-const abreAlerta = (mensagem,cor)=>{
-     
+const abreAlerta = (mensagem, cor) => {
+
     alerta.innerText = mensagem
     alerta.classList.add('mostrar')
-    alerta.style.backgroundColor= cor
-    alerta.style.display="flex"
-    setTimeout(()=> {
+    alerta.style.backgroundColor = cor
+    alerta.style.display = "flex"
+    setTimeout(() => {
         alerta.classList.remove('mostrar')
-      }, 3000)
+    }, 3000)
 
 }
 
@@ -78,13 +81,13 @@ const fecharCard = (div) => {
 }
 const abreCard = (div) => {
     div[0].classList.remove("display_off")
-   
+
 }
 
 const criaLinha = (usuario, i) => {
 
     const novaLinha = document.createElement('div')
-    novaLinha.setAttribute("id","linhaJogador")
+    novaLinha.setAttribute("id", "linhaJogador")
     novaLinha.innerHTML = `
     <div id="divNomePontuacao" >${usuario.nome}</div>
     <div class="btnPontuacao" onclick="SelecionaJogador(${i})") ><ion-icon name="football"></ion-icon></div>
@@ -92,30 +95,46 @@ const criaLinha = (usuario, i) => {
     <ion-icon class="btn_delet" onclick="excluir(${i})" name="close-circle"></ion-icon>  
     `
 
-    corpoTabela.appendChild(novaLinha)
+    corpoTabelaCadastro.appendChild(novaLinha)
+}
+
+const criaLinhaRanking = (usuario, i) => {
+
+    const novaLinha = document.createElement('div')
+    novaLinha.setAttribute("id", "linhaRanking")
+    novaLinha.innerHTML = `
+        <div id="colocacaoRanking">${i+1}</div>
+        <div id="nomeRanking">${usuario.nome}</div>
+        <div id="boxPontuacaoRanking">
+            <div id="valorPontuacaoRanking">${usuario.total}</div>
+            <span>pontos</span>
+        </div>
+    `
+
+    corpoTabelaRanking.appendChild(novaLinha)
 }
 
 
 const somaPontuacao = (index) => {
     const usuarioDb = getLocalStorage()
     const valorPontos = {
-        gol:8,
-        assist:5,
-        desarme:2,
-        defesa:4
+        gol: 8,
+        assist: 5,
+        desarme: 2,
+        defesa: 4
     }
 
     usuarioDb.forEach(
         (el, i) => {
 
             if (index == i) {
-                
-               el.total = el.gol*valorPontos.gol+el.assist* valorPontos.assist+el.desarme*valorPontos.desarme+el.defesa*valorPontos.defesa
+
+                el.total = el.gol * valorPontos.gol + el.assist * valorPontos.assist + el.desarme * valorPontos.desarme + el.defesa * valorPontos.defesa
 
             }
         }
     )
-     setLocalStorage(usuarioDb)
+    setLocalStorage(usuarioDb)
 }
 
 const criaDiv = (i) => {
@@ -140,9 +159,9 @@ const btnDiminuir = (metodo) => {
     metodo.stepDown()
 }
 
-const trocaPagina = (paginaAtual,...pagOff) => {
+const trocaPagina = (paginaAtual, ...pagOff) => {
     paginaAtual.style.display = "flex"
-    pagOff.forEach((el)=>{el.style.display ="none"})
+    pagOff.forEach((el) => { el.style.display = "none" })
 }
 
 const SelecionaJogador = (index) => {
@@ -185,7 +204,7 @@ const atualizaJogador = (e) => {
             }
         }
     )
-    abreAlerta("Jogador Atualizado","#3fd075")
+    abreAlerta("Jogador Atualizado", "#3fd075")
 }
 
 const excluir = (e) => {
@@ -196,11 +215,11 @@ const excluir = (e) => {
             if (e == i) {
                 usuarioDb.splice(i, 1)
                 setLocalStorage(usuarioDb)
-                corpoTabela.childNodes[i].remove()
+                corpoTabelaCadastro.childNodes[i].remove()
             }
         }
     )
-    abreAlerta("Jogador Excluido","#e65555")
+    abreAlerta("Jogador Excluido", "#e65555")
     atualizaTabela()
 }
 
@@ -223,12 +242,12 @@ function salvar() {
         registraUsuario(usuario)
         atualizaTabela()
         nomeCadastro.value = ""
-        abreAlerta("Jogador Salvo","#3fd075")
+        abreAlerta("Jogador Salvo", "#3fd075")
     }
 
 }
 
-const limpaTabela = () => {
+const limpaTabela = (corpoTabela) => {
     while (corpoTabela.firstChild) {
         corpoTabela.removeChild(corpoTabela.lastChild)
     }
@@ -236,15 +255,15 @@ const limpaTabela = () => {
 
 const atualizaTabela = () => {
     const usuarioDb = getLocalStorage()
-    limpaTabela()
+    limpaTabela(corpoTabelaCadastro)
     textoDinamico()
     usuarioDb.forEach(criaLinha)
     usuarioDb.forEach(trocaCorBtnPontuacao)
 
 }
 
-const trocaCorBtnPontuacao = (usuario,index)=>{
-    if (usuario.total>0) {btnPontuacao[index].style.color="#3fd075"}
+const trocaCorBtnPontuacao = (usuario, index) => {
+    if (usuario.total > 0) { btnPontuacao[index].style.color = "#3fd075" }
 }
 
 const textoDinamico = () => {
